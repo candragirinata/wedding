b12117427af338f2f6b1d9a9f80cbfb1e2767cc9
giri <ghirinata@gmail.com>
2021-01-28 16:11:36 +0800

    asds

--- resources/views/beranda/beranda_index.blade.php
+++ resources/views/beranda/beranda_index.blade.php
@@ -86,7 +86,7 @@
 	?>
 
 	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url({{ asset('uploads/'.$banner->gambar) }});background-size: 100%;" data-stellar-background-ratio="0.5">
-		<div class="overlay"></div>
+		{{-- <div class="overlay"></div> --}}
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center">
@@ -101,18 +101,20 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		{{-- </div> --}}
 	</header>
 
 	<?php
 		$penganten = \DB::table('penganten')->first();
 	?>
 
-	<div id="fh5co-couple">
+	<div id="fh5co">
 		<div class="container">
 			<div class="row">
+			
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-					<h2>{{ $penganten->salam }}</h2>
+					<h1>Om Swastiastu</h1>
+					<h6>{{ $penganten->salam }}</h6>
 					<h3>{{ $penganten->tanggal }}</h3>
 					<p>{{ $penganten->title }}</p>
 				</div>
@@ -120,20 +122,20 @@
 			<div class="couple-wrap animate-box">
 				<div class="couple-half">
 					<div class="groom">
-						<img src="{{asset('uploads/'.$penganten->gambar1)}}" alt="groom" class="img-responsive">
+						<a href="https://www.instagram.com/ayuyulilestari2/"><img src="{{asset('uploads/'.$penganten->gambar1)}}" alt="groom" class="img-responsive"></a>
 					</div>
 					<div class="desc-groom">
-						<h3>{{ $penganten->nama1 }}</h3>
+						<a href="https://www.instagram.com/ayuyulilestari2/"><h3>{{ $penganten->nama1 }}</h3></a>
 						<p>{{ $penganten->keterangan1 }}</p>
 					</div>
 				</div>
 				<p class="heart text-center"><i class="icon-heart2"></i></p>
 				<div class="couple-half">
 					<div class="bride">
-						<img src="{{asset('uploads/'.$penganten->gambar2)}}" alt="groom" class="img-responsive">
+						<a href="https://www.instagram.com/candragirinata/"><img src="{{asset('uploads/'.$penganten->gambar2)}}" alt="groom" class="img-responsive"></a>
 					</div>
 					<div class="desc-bride">
-						<h3>{{ $penganten->nama2 }}</h3>
+						<a href="https://www.instagram.com/candragirinata/"><h3>{{ $penganten->nama2 }}</h3></a>
 						<p>{{ $penganten->keterangan2 }}</p>
 					</div>
 				</div>
@@ -141,40 +143,46 @@
 		</div>
 	</div>
 
-	<div id="fh5co-event" class="fh5co-bg" style="background-image:url({{asset('wedding/images/img_bg_3.jpg')}});">
+	<div id="fh5co-event" class="fh5co-bg" style="background-image:url({{asset('wedding/images/acara.jpg')}});">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
 					<!-- <span>Our Special Events</span> -->
 
-					<h2>{{ \DB::table('acara_title')->where('acara_title_id',1)->value('title') }}</h2>
+					<h2 style="rgb(228 194 121)">{{ \DB::table('acara_title')->where('acara_title_id',1)->value('title') }}</h2>
 				</div>
 			</div>
 			<?php
 				$akad = \DB::table('acara_akad')->first();
 				$resepsi = \DB::table('acara_resepsi')->first();
 			?>
+
+			
 			<div class="row">
 				<div class="display-t">
 					<div class="display-tc">
 						<div class="col-md-10 col-md-offset-1">
-							<div class="col-md-6 col-sm-6 text-center">
+							{{-- <div class="col-md-6 col-sm-6 text-center"> --}}
 								<div class="event-wrap animate-box">
 									<h3>{{ $akad->nama }}</h3>
 									<div class="event-col">
-										<i class="icon-clock"></i>
-										<span>{{ $akad->jam1 }}</span>
-										<span>{{ $akad->jam2 }}</span>
+										<i class="icon-clock"> </i> <p style="color:rgb(255, 255, 255)">  Pukul : <br>
+										{{ $akad->jam1 }} - 
+										{{ $akad->jam2 }} </p>
+										{{-- <span>{{ $akad->jam1 }}</span>
+										<span>{{ $akad->jam2 }}</span> --}}
 									</div>
 									<div class="event-col">
-										<i class="icon-calendar"></i>
-										<span>{{ $akad->tanggal1 }}</span>
-										<span>{{ $akad->tanggal2 }}</span>
+										<i class="icon-calendar"></i> <p style="color:rgb(255, 255, 255)">  Hari/Tanggal : <br>
+										{{ $akad->tanggal1 }} ,
+										{{ $akad->tanggal2 }} </p>
+										{{-- <span>{{ $akad->tanggal1 }}</span>
+										<span>{{ $akad->tanggal2 }}</span> --}}
 									</div>
 									<p>{{ $akad->keterangan }}</p>
 								</div>
-							</div>
+							{{-- </div> --}}
 							{{-- <div class="col-md-6 col-sm-6 text-center">
 								<div class="event-wrap animate-box">
 									<h3>{{ $resepsi->nama }}</h3>
@@ -242,16 +250,101 @@
 		</div>
 	</div>
 
-	<div id="fh5co-services" class="fh5co-section-gray">
+	{{-- <div id="fh5co-services" class="fh5co-section-gray"> --}}
 		<div class="container">
 			
-			<div class="row animate-box">
+			<div class="event-wrap row animate-box">
 
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
 					{{-- <h2>{{ \DB::table('jasa_title')->value('title') }}</h2> --}}
+					
+					{{-- ---------------------------------------------- --}}
+					<h2>Our Video</h2>
+					{{-- ---------------------------------------------- --}}
+					<iframe src="https://www.youtube.com/embed/5ikBbwzqvew" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<br><br><br>
+							
+					
 
+					{{-- ---------------------------------------------- --}}
+					<h2>Our Gallery</h2>
+					{{-- ---------------------------------------------- --}}
+					<div class="row">
+						<div class="col-md-4">
+						  <div class="thumbnail">
+							<a href="uploads/1.jpg">
+							  <img src="uploads/1.jpg" alt="" style="width:100%">
+							  {{-- <div class="caption">
+								<p>Lorem ipsum...</p>
+							  </div> --}}
+							</a>
+						  </div>
+						</div>
+						<div class="col-md-4">
+						  <div class="thumbnail">
+							<a href="uploads/2.jpg">
+							  <img src="uploads/2.jpg" alt="" style="width:100%">
+							  {{-- <div class="caption">
+								<p>Lorem ipsum...</p>
+							  </div> --}}
+							</a>
+						  </div>
+						</div>
+						<div class="col-md-4">
+						  <div class="thumbnail">
+							<a href="uploads/3.jpg">
+							  <img src="uploads/3.jpg" alt="" style="width:100%">
+							  {{-- <div class="caption">
+								<p>Lorem ipsum...</p>
+							  </div> --}}
+							</a>
+						  </div>
+						</div>
+					  </div>
+
+					  <div class="row">
+						<div class="col-md-4">
+						  <div class="thumbnail">
+							<a href="uploads/4.jpg">
+							  <img src="uploads/4.jpg" alt="" style="width:100%">
+							  {{-- <div class="caption">
+								<p>Lorem ipsum...</p>
+							  </div> --}}
+							</a>
+						  </div>
+						</div>
+						<div class="col-md-4">
+						  <div class="thumbnail">
+							<a href="uploads/5.jpg">
+							  <img src="uploads/5.jpg" alt="" style="width:100%">
+							  {{-- <div class="caption">
+								<p>Lorem ipsum...</p>
+							  </div> --}}
+							</a>
+						  </div>
+						</div>
+						<div class="col-md-4">
+						  <div class="thumbnail">
+							<a href="uploads/6.jpg">
+							  <img src="uploads/6.jpg" alt="" style="width:100%">
+							  {{-- <div class="caption">
+								<p>Lorem ipsum...</p>
+							  </div> --}}
+							</a>
+						  </div>
+						</div>
+					  </div>
+
+
+
+
+
+				
+					<br><br><br>
+					{{-- ---------------------------------------------- --}}
 					<h2>Ceremony Location</h2>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d504900.0744275998!2d114.93607802386134!3d-8.640861452838998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd22f46b5370dab%3A0xe56196e191bc8b3e!2sSumerta%20House!5e0!3m2!1sen!2sid!4v1605532075582!5m2!1sen!2sid" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+					{{-- ---------------------------------------------- --}}
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d504900.0744275998!2d114.93607802386134!3d-8.640861452838998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd22f46b5370dab%3A0xe56196e191bc8b3e!2sSumerta%20House!5e0!3m2!1sen!2sid!4v1605532075582!5m2!1sen!2sid" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 					
 				</div>
 			</div>
@@ -293,7 +386,7 @@
 
 			
 		</div>
-	</div>
+	{{-- </div> --}}
 
 	<footer id="fh5co-footer" role="contentinfo">
 		<div class="container">
@@ -301,17 +394,19 @@
 			<div class="row copyright">
 				<div class="col-md-12 text-center">
 					<p>
-						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FREEHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
-						<small class="block">Modified by <a href="http://sangcahaya.com/" target="_blank">sangcahaya.com</a> | Fadly Shafa</small>
+						<small class="block">&copy; 2020 Free HTML5. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FREEHTML5.co</a></small>
+						<small class="block">Pengantin : <a href="https://www.instagram.com/candragirinata/" target="_blank">Giri</a> & <a href="https://www.instagram.com/ayuyulilestari2/" target="_blank">Ayu</a></small>
+
 					</p>
 					<p>
-						<ul class="fh5co-social-icons">
-							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
+						{{-- <ul class="fh5co-social-icons"> --}}
+							{{-- <li><a href="#"><i class="icon-twitter"></i></a></li> --}}
+							{{-- <li><a href="#"><i class="icon-facebook"></i></a></li>
 							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
-						</ul>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li> --}}
+						{{-- </ul> --}}
+					
 					</p>
 				</div>
 			</div>
@@ -321,7 +416,7 @@
 	</div>
 
 	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+		<a href="#" class="js-gotop"><i class="icon-arrow-up">Auto Scroll</i></a>
 	</div>
 	
 	<!-- jQuery -->
